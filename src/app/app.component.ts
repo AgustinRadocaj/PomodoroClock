82cee3e0cb98bf03289310b26087d42c78a6b300
Agustin Radocaj <jaguscaj@gmail.com>
2023-11-30 13:23:54 -0300

    Cambio botones - Sumar minutos a las sesiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,45 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'PomodoroClock';
+  break: number = 5;
+  session: number = 25;
+  work: boolean = true;
+  isRunning: boolean = false;
+  timer: any = 25;
+
+  breakHandler(param: number) {
+    if(param === 1 && this.break <= 24) this.break += 1
+    if(param === 0 && this.break >= 2) this.break -= 1
+  } 
+
+  sessionHandler(param: number) {
+    if(param === 1 && this.session <= 59) this.session += 1
+    if(param === 0 && this.session >= 16) this.session -= 1
+  }
+
+  reset() {
+    this.break = 5;
+    this.session = 25;
+  }
+  
+  timerHandler() {
+    if(!this.isRunning){
+      this.timerStart()
+    } else {
+      this.timerStop()
+    }
+  } 
+      
+      timerStart() {
+        this.isRunning = true;
+        this.timer = setInterval(() => this.timer = this.timer - 1, 1000)
+      }
+
+      timerStop() {
+        this.isRunning = false;
+        clearInterval(this.timer)
+      }
 }
+  
+  
+
